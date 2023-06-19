d17034dc58aea4744d6ceeac4c63c7aa22e31a2e
CodexK7 <nkrumahisaac2004@gmail.com>
2023-06-19 03:01:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
    * main - Entry point
@@ -8,5 +9,5 @@
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	    return (0);
+	return (0);
 }
